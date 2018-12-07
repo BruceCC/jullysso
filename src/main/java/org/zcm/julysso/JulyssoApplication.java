3885373993030bcc7628fbe33ce409d913e20cc8
BruceCC <1804284906@qq.com>
2018-12-07 15:02:00 +0800

    init

---
+++ src/main/java/org/zcm/julysso/JulyssoApplication.java
@@ -0,0 +1,12 @@
+package org.zcm.julysso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulyssoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JulyssoApplication.class, args);
+	}
+}
---
+++ src/test/java/org/zcm/julysso/JulyssoApplicationTests.java
@@ -0,0 +1,16 @@
+package org.zcm.julysso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JulyssoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
